72620fdedba9518020fe77ddd45695db3173301b
Gwillis442 <122920316+Gwillis442@users.noreply.github.com>
2023-11-09 19:05:18 -0800

    starting repository for discord bot

---
+++ RandomFunBot.js
@@ -0,0 +1,81 @@
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ 
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages
+  ]
+});
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+  });
+
+// Set the username to target for message deletion
+const targetUsername = "kony911";
+
+client.on('messageCreate', (message) => {
+  
+  // Check if the message author's username matches the target username
+  if (message.author.username === targetUsername) {
+    // Generate a random number between 1 and 100
+    const randomNumber = Math.floor(Math.random() * 150) + 1;
+
+    // If the random number is 1, delete the message
+    if (randomNumber === 1) {
+      message.delete()
+      .then(deletedMessage => {
+        console.log(`Deleted message from ${deletedMessage.author.tag}: ${deletedMessage.content}`);
+        // Send a DM to the message author
+        //message.channel.send(`Your message was deleted ${message.author}`);
+    
+      })
+      .catch(console.error);
+    }
+  }
+});
+
+//If message author's username does not match target
+client.on('messageCreate', (message) =>{
+  if(message.author.username !== targetUsername){
+    const randomNumber3 = Math.floor(Math.random() * 200) + 1;
+
+     // If the random number is 1, delete the message
+    if (randomNumber3 === 1) {
+      message.delete()
+      .then(deletedMessage => {
+      console.log(`Deleted message from ${deletedMessage.author.tag}: ${deletedMessage.content}`);
+
+      });
+    }
+  }
+});
+
+client.on('messageCreate', (message) => {
+  
+  const randomNumber1 = Math.floor(Math.random() * 100) + 1;
+
+  if (randomNumber1 === 1){
+
+      message.react('👍');
+
+  } else if(randomNumber1 === 3){
+
+      message.react('👎');
+
+  }    
+});  
+
+client.on('messageCreate', (message) =>{
+
+    const randomNumber2 = Math.floor(Math.random() * 2048) + 1;
+
+    if(randomNumber2 === 1){
+      
+      message.channel.send(`WARNING WARNING, ${message.author} IS REQUIRED TO ATTEND A MANDATORY PEBIS INSPECTION, NON COMPLIANCE WILL RESULT IN TERMINATION, PLEASE HEAD TO THE PEBIS EXTENDER ROOM IMMEDIATELY`)
+
+    }
+
+});
+
+client.login('MTE3MjAyNTUwOTU3MjUzMDIyNg.Gbg__A.kmQAj7zl07nvB9pUCzUSRhXQ2jW088XNFzcUxw');
