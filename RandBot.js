0c0d7d429c2d9ec9f826b218cd73e6aa1a4ef543
Gwillis442 <122920316+Gwillis442@users.noreply.github.com>
2023-11-14 10:32:31 -0800

    updated discordjs

--- RandBot.js
+++ RandBot.js
@@ -11,7 +11,7 @@ const client = new Client({
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
-    testRNG();
+    //testRNG();
   });
 
 // Set the username to target for message deletion
