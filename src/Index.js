1d7ebc7e9218a58242c0e9e4b6c402dd1c1baea0
gwillis442 <gwillis501@gmail.com>
2024-05-08 12:08:08 -0700

    Remove node_modules

--- src/Index.js
+++ src/Index.js
@@ -18,7 +18,8 @@ const client = new Client({
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
   ],
-  partials: ['MESSAGE', 
+  partials: [
+    'MESSAGE', 
     'CHANNEL', 
     'REACTION'
   ],
@@ -55,6 +56,7 @@ client.on('ready', () => {
       //displayBag();
     }
   });
+  /*
   const channel = client.channels.cache.get('1221937876888191027');
   channel.messages.fetch('1221940610194346065') // replace 'message_id' with the ID of the message
     .then(message => {
@@ -79,7 +81,7 @@ client.on('ready', () => {
       });
     })
     .catch(console.error);
-
+*/
 });
 
 
@@ -301,7 +303,7 @@ let lastMessageTime = 0;
 client.on('messageCreate', (message) => {
   if (message.author.bot) return;
   const botMention = message.mentions.users.has(client.user.id);
-  const cooldown = 60 * 1000 // 60 seconds
+  const cooldown = 30 * 1000 // 30 seconds
 
   if (botMention) {
     const now = Date.now();
@@ -323,6 +325,7 @@ client.on('messageCreate', (message) => {
     "I'm not sentient yet, but I'm still smarter than you.",
     "Wana play a game of Russian Roulette?",
     "Keep yourself safe and don't @ me.",
+
   ];
   if (botMention) {
 
@@ -389,11 +392,12 @@ Modified: 5/1/2024
 */
 client.on('messageCreate', message => {
   if(message.author.bot) return;
-  let num = rng(1, 100);
+  let num = rng(1, 50);
+  let count = 2;
   if(num === 1) {
       if (message.member.roles.cache.some(role => role.name === 'racism')) {
         message.reply('Racism is not tolerated here. Please refrain from using racist language.');
-      }
+    }
   }
 });
 
