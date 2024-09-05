61d09458dc7e5d9d57cdddde3c8abdb507011905
Gwillis442 <122920316+Gwillis442@users.noreply.github.com>
2024-09-05 10:45:32 -0700

    randomResponseHandler added

--- constants/arrays.js
+++ constants/arrays.js
@@ -39,5 +39,22 @@ const emojiArray = [
 
     let userBag = [];
 
+    const gifArray = [
+      'https://tenor.com/view/death-stare-black-snake-moan-samuel-l-jackson-shocked-wide-eye-gif-14648637',
+      'https://tenor.com/view/reikouwu2-gif-20327386',
+      'https://tenor.com/view/ew-gif-25919594',
+      'https://tenor.com/view/sweating-nervous-wreck-gif-24688521',
+      'https://tenor.com/view/josh-hutcherson-josh-hutcherson-whistle-edit-whistle-2014-meme-gif-1242113167680346055',
+      'https://tenor.com/view/future-josh-hutcherson-tongue-out-gif-13846119',
+      'https://tenor.com/view/doubt-press-x-la-noire-meme-x-button-gif-19259237',
+      'https://tenor.com/view/if-you-say-so-ok-gif-9410059',
+      'https://tenor.com/view/ohhh-duh-why-didnt-i-think-of-that-gif-21849807',
+      'https://tenor.com/view/snoop-dogg-dance-moves-yes-gif-16124908',
+      'https://tenor.com/view/confused-no-nope-gif-5413974953753901704',
+      `https://tenor.com/view/jarvis-iron-man-goon-gif-5902471035652079804`,
+      'https://tenor.com/view/snoop-dogg-rapper-raper-angry-anger-gif-17002797',
 
-module.exports = { emojiArray, johnArray, admin, userBag };
+    ];
+
+
+module.exports = { emojiArray, johnArray, admin, userBag, gifArray };
