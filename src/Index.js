ea3ca1a52758e11cd1549a59cae82248c49e7195
gwillis442 <gwillis501@gmail.com>
2024-09-05 10:50:49 -0700

    renamed Index.js -> index.js

--- src/Index.js
+++
@@ -1,4 +0,0 @@
-require('./client.js');
-require('./handlers/messageHandlers/linkHandler.js');
-require('./handlers/messageHandlers/messageDeletionHandler.js');
-require('./handlers/messageHandlers/randomResponseHandler.js');
