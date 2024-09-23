2f18a8d0789228e4717edc930ba14691b136ad37
Gwillis442 <122920316+Gwillis442@users.noreply.github.com>
2024-09-23 13:45:02 -0700

    added message history

---
+++ src/handlers/messageHandlers/messageHistoryHandler.js
@@ -0,0 +1,33 @@
+//stores a message history for a specific channel
+//stores the last 100 messages
+const { client } = require('../../client.js');
+const { mainChannelId } = require('../../../config/config.json');
+
+const messageHistory = new Map();
+
+function addMessageToHistory(message, username) {
+
+    //if the message history is full, remove the oldest message
+    if (messageHistory.size >= 100) {
+        messageHistory.delete(messageHistory.keys().next().value);
+    }
+
+    //add the message to the history
+    messageHistory.set(message.id, {
+        username: username,
+        content: message.content,
+        timestamp: message.createdTimestamp
+    });
+
+}
+
+//get the message history for the channel
+function getMessageHistory() {
+    return messageHistory;
+}
+
+client.on('messageCreate', message => {
+    if (mainChannelId != message.channelId) return;
+    addMessageToHistory(message, message.author.username);
+});
+
