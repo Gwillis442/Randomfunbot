5c4f7a8fe0bd6646382e668eeb728100ef39b8de
gwillis442 <gwillis501@gmail.com>
2024-11-12 18:38:03 -0800

    fixed updateDatabase.js

--- src/utils/updateDatabase.js
+++ src/utils/updateDatabase.js
@@ -5,6 +5,7 @@
 // table: the table to update
 // row: the row to update
 // value: the value to update the row with
+
 function update_Database(db, table, row, value, user) {
   db.run(`UPDATE ${table} SET ${row} = ${value} WHERE user_id = ${user}`, function(err) {
     if (err) {
